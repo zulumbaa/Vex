c06dde4e6587a07ad313cd53bfb2f860f9fee903
zulumbaa <122629491+zulumbaa@users.noreply.github.com>
2025-07-24 22:05:23 +0300

    Changed MC sprite, add light sooh

--- Assets/Scriprs/Vex.cs
+++ Assets/Scriprs/Vex.cs
@@ -15,14 +15,22 @@ public class Vex : MonoBehaviour
     private float moveX = 0f;
     private float moveY = 0f;
     private float lastMoveX = 0f;
-    private float lastMoveY = -1f; 
+    private float lastMoveY = -1f;
+
+    public GameObject lightBeam; 
+    public Camera mainCamera;
+
+    private bool isShooting = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
-
+    void Update()
+    {
+        TestShooting();
+    }
     void FixedUpdate()
     {
         Move();
@@ -33,13 +41,25 @@ public class Vex : MonoBehaviour
         float newMoveX = Input.GetAxisRaw("Horizontal");
         float newMoveY = Input.GetAxisRaw("Vertical");
 
-        if (newMoveX != 0 || newMoveY != 0) 
+        if (newMoveX != 0 || newMoveY != 0)
+        {
+            lastMoveX = newMoveX;
+            lastMoveY = newMoveY;
+        }
+
+        if (isShooting)
+        {
+            rb.velocity = Vector2.zero;
+            animator.SetBool("IsMoving", false);
+            animator.SetFloat("MoveX", lastMoveX);
+            animator.SetFloat("MoveY", lastMoveY);
+            return;
+        }
+
+        if (newMoveX != 0 || newMoveY != 0)
         {
             moveX = newMoveX;
             moveY = newMoveY;
-
-            lastMoveX = moveX;
-            lastMoveY = moveY;
 
             Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
             rb.velocity = moveDirection * moveSpeed;
@@ -48,7 +68,7 @@ public class Vex : MonoBehaviour
             animator.SetFloat("MoveY", moveY);
             animator.SetBool("IsMoving", true);
         }
-        else 
+        else
         {
             rb.velocity = Vector2.zero;
             animator.SetBool("IsMoving", false);
@@ -58,6 +78,46 @@ public class Vex : MonoBehaviour
         }
     }
 
+    private void TestShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isShooting = true;
+
+            Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0f;
+            Vector3 direction = mouseWorldPos - lightBeam.transform.position;
+
+            Vector2 normalizedDirection = direction.normalized;
+            Vector2 roundedDir = new Vector2(
+                Mathf.Round(normalizedDirection.x),
+                Mathf.Round(normalizedDirection.y));
+
+            animator.SetFloat("LightShootX", roundedDir.x);
+            animator.SetFloat("LightShootY", roundedDir.y);
+
+            animator.SetFloat("MoveX", lastMoveX);
+            animator.SetFloat("MoveY", lastMoveY);
+
+            animator.SetBool("WasShooted", true);
+
+            lightBeam.SetActive(true);
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            lightBeam.transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
+
+            StartCoroutine(ResetShootFlag());
+        }
+    }
+
+    IEnumerator ResetShootFlag()
+    {
+        yield return new WaitForSeconds(0.5f); 
+        animator.SetBool("WasShooted", false);
+        lightBeam.SetActive(false);
+        isShooting = false;
+
+    }
     private void OnDamage(float damage)
     {
         curr_health_index -= damage;
